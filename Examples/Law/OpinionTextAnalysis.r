1f3b651885d6ea6920b8d1d2b64255e563567ebf
Tom Balmat <38592571+tbalmat@users.noreply.github.com>
2019-10-13 22:46:52 -0400

    Delete OpinionTextAnalysis.r

--- Examples/Law/OpinionTextAnalysis.r
+++
@@ -1,7 +0,0 @@
-library("shiny")
-
-runApp(appDir="C:\\Projects\\Duke\\Law\\LexisNexisCaseAnalysis\\LexisNexisDecisionSample\\OpinionTextAnalysis\\OpinionTextAnalysisApp",
-       launch.browser=T,
-       host = getOption("shiny.host", "127.0.0.1"),
-       #port=4291,
-       display.mode = c("auto", "normal", "showcase"))
