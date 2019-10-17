58f7b119be3937ff6ae45f7ee1c12b3f47914716
Tom Balmat <38592571+tbalmat@users.noreply.github.com>
2019-10-17 03:32:20 -0400

    Update CPDF.r

--- Session-1/App/CPDF/ShellExecution/CPDF-Shiny-1/CPDF.r
+++ Session-1/App/CPDF/ShellExecution/CPDF-Shiny-1/CPDF.r
@@ -1,6 +1,6 @@
 library("shiny")
 
-runApp(appDir="C:\\Projects\\Duke\\Co-lab\\Shiny\\Session-1-NPDHist-CPDF\\App\\CPDF\\ShellExecution",
+runApp(appDir="C:\\Projects\\Duke\\Co-lab\\Shiny\\Session-1-NPDHist-CPDF\\App\\CPDF\\ShellExecution\\CPDF-Shiny-1",
        launch.browser=T,
        host = getOption("shiny.host", "127.0.0.1"),
        #port=4291,
